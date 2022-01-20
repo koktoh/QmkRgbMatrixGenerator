28a1ce41a15b66a766f6253cc3b6dbf4ac3751f3
koktoh <koktoh96@gmail.com>
2022-01-20 22:03:19 +0900

    add extensions

---
+++ QmkRgbMatrixGenerator/Extensions/LinqExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace QmkRgbMatrixGenerator.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<IGrouping<int, T>> GroupByIndex<T>(this IEnumerable<T> source, int index)
+        {
+            return source.Select((x, i) => new { Index = i, Value = x }).GroupBy(x => x.Index / index, x => x.Value);
+        }
+    }
+}
---
+++ QmkRgbMatrixGenerator/Extensions/StringExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QmkRgbMatrixGenerator.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string source)
+        {
+            return string.IsNullOrWhiteSpace(source);
+        }
+
+        public static bool HasMeaningfulValue(this string source)
+        {
+            return !string.IsNullOrWhiteSpace(source);
+        }
+
+        public static string Join(this IEnumerable<string> source, string separator)
+        {
+            return string.Join(separator, source);
+        }
+
+        public static string JoinComma(this IEnumerable<string> source, bool trailWhiteSpace = false)
+        {
+            var separator = trailWhiteSpace ? ", " : ",";
+
+            return source.Join(separator);
+        }
+
+        public static string JoinNewLine(this IEnumerable<string> source)
+        {
+            return source.Join(Environment.NewLine);
+        }
+
+        public static IEnumerable<string> SplitComma(this string source, StringSplitOptions options = StringSplitOptions.None)
+        {
+            return source.Split(",", options);
+        }
+
+        public static IEnumerable<string> SplitNewLine(this string source, StringSplitOptions options = StringSplitOptions.None)
+        {
+            return source.Split(Environment.NewLine, options);
+        }
+
+        public static string Trim(this string source, string trimStr)
+        {
+            return source.Trim(trimStr.ToCharArray());
+        }
+
+        public static string TrimStart(this string source, string trimStr)
+        {
+            return source.TrimStart(trimStr.ToCharArray());
+        }
+
+        public static string TrimEnd(this string source, string trimStr)
+        {
+            return source.TrimEnd(trimStr.ToCharArray());
+        }
+
+        public static string Escape(this string source)
+        {
+            return source?.Replace(@"\", @"\\").Replace(@"""", @"\""");
+        }
+    }
+}
